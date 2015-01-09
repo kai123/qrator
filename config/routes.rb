5744d3d33f2dc549934fd214c898b7be59e05049
Kai Holländer <kai@videri-concept.de>
2015-01-09 16:00:25 +0100

    kleine Änderungen Startseite (unfertig)

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   
-  #root 'qr_codes#new'
-  root 'qrcode#generator'
+  root 'qr_codes#new'
+  #root 'qrcode#generator'
 
   # get 'senden' => 'qr_codes#index'
 
