81051069d2317bd4a7796943d3ff6576227e402b
Kai Holländer <kai@videri-concept.de>
2015-01-15 17:56:19 +0100

    Qr-code eingefuegt
    QR code wird erstellt, aber noch nicht angezeigt

--- app/controllers/qr_codes_controller.rb
+++ app/controllers/qr_codes_controller.rb
@@ -1,32 +1,42 @@
 class QrCodesController < ApplicationController
 	before_action :set_qr_code, only: [:show, :edit, :update, :destroy]
+	
+	def renderQr
+		@qrcode = RQRCode::QRCode.new( @qr_code.url, :size => 4, :level => :h )	
+		puts @qrcode
+		puts @qrcode.inspect	
+	end
 
 	def index
-		@qr_codes = QrCode.all
+		@qr_codes = QrCode.all		
 	end
 
 	def show
-		@qr = QrCode.find(params[:id])
+		@qrs = QrCode.find(params[:id])		
 	end
 
 	def new
-		@qr_code = QrCode.new(url: "http://")
+		@qr_code = QrCode.new(url: "http://")			
 	end
 
 	def edit
+		@qr_code = QrCode.find(params[:id])
+		renderQr		
 	end
 
-	def create
+	def create		
 		@qr_code = QrCode.new(qr_code_params)
-		if @qr_code.save
+		if @qr_code.save		
+			renderQr	
 			redirect_to qr_codes_path, notice: 'Qr code was successfully created.'
 		else
 			render :new
-		end
+		end		
 	end
 
 	def update
-		if @qr_code.update(qr_code_params)
+		if @qr_code.update(qr_code_params)		
+			renderQr	
 			redirect_to @qr_code, notice: 'Qr code was successfully updated.'
 		else
 			render :edit
